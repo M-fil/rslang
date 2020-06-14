8414557a9a365a346617579a1f2da236c945a06e
Maxim Filanovich <maxim_filanovich@mail.ru>
2020-06-14 14:47:38 +0300

    refactor: add components folder for scss files and utils for extra js files

---
+++ rslang/src/js/utils/сreate.js
@@ -0,0 +1,36 @@
+export default function create(el, classNames, child, parent, ...dataAttr) {
+  let element = null;
+  try {
+    element = document.createElement(el);
+  } catch (error) {
+    throw new Error('Unable to create HTMLElement! Give a proper tag name');
+  }
+
+  if (classNames) element.classList.add(...classNames.split(' '));
+
+  if (child && Array.isArray(child)) {
+    child.forEach((childElement) => childElement && element.appendChild(childElement));
+  } else if (child && typeof child === 'object') {
+    element.appendChild(child);
+  } else if (child && typeof child === 'string') {
+    element.innerHTML = child;
+  }
+
+  if (parent) {
+    parent.appendChild(element);
+  }
+
+  if (dataAttr.length) {
+    dataAttr.forEach(([attrName, attrValue]) => {
+      if (attrValue === '') {
+        element.setAttribute(attrName, '');
+      }
+      if (attrName.match(/value|href|target|id|title|placeholder|cols|rows|autocorrect|type|spellcheck/)) {
+        element.setAttribute(attrName, attrValue);
+      } else {
+        element.dataset[attrName] = attrValue;
+      }
+    });
+  }
+  return element;
+}
