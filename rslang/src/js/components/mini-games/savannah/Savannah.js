80a55001c80c6b4d4583ec2d6737c9f3249843f9
Ekaterina Latushkina <katia_sali@mail.ru>
2020-06-16 14:47:51 +0300

    feat: add reverse report

--- rslang/src/js/components/mini-games/savannah/Savannah.js
+++ rslang/src/js/components/mini-games/savannah/Savannah.js
@@ -12,6 +12,7 @@ export default class SavannahGame {
     this.body = document.querySelector('body');
     this.container = create('div', 'container', '', this.body);
     this.gameWindow = create('div', 'start-game-window', '', this.container);
+    this.numberReverse = create('span', 'number-reverse', '', this.body);
   }
 
   render() {
@@ -20,5 +21,26 @@ export default class SavannahGame {
     this.HTML = create('button', 'start-button', START_BUTTON, this.gameWindow);
     this.HTML = create('button', 'exit-button', 'X', this.body);
     return this.HTML;
+  }
+
+  reverseReport() {
+    const startGameWindow = document.querySelector('.start-game-window');
+    startGameWindow.style.display = 'none';
+    this.numberReverse.style.display = 'block';
+    const seconds = 3;
+    let timeLeft = seconds;
+    const timer = setInterval(() => {
+      if (timeLeft > 0) {
+        this.numberReverse.innerHTML = timeLeft;
+        timeLeft -= 1;
+      } else {
+        clearInterval(timer);
+        this.mainGame();
+      }
+    }, 1000);
+  }
+
+  mainGame() {
+    this.numberReverse.style.display = 'none';
   }
 }
