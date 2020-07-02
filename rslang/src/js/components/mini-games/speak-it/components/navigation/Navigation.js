e1b4761526e6486237fa5f098b69f63255087951
Maxim Filanovich <maxim_filanovich@mail.ru>
2020-07-02 16:03:48 +0300

    feat: add styles and label for groups select list

--- rslang/src/js/components/mini-games/speak-it/components/navigation/Navigation.js
+++ rslang/src/js/components/mini-games/speak-it/components/navigation/Navigation.js
@@ -3,6 +3,7 @@ import create, { speakItConstants } from '../../pathes';
 const {
   DESCRIPTION_OF_LEVELS,
   NUMBER_OF_GROUPS,
+  GROUPS_OF_WORDS_TEXT,
 } = speakItConstants;
 
 export default class Navigation {
@@ -12,7 +13,9 @@ export default class Navigation {
 
   render() {
     this.container = create('div', 'navigation__list');
-    this.select = create('select', 'navigation__group-select', '', this.container, ['title', DESCRIPTION_OF_LEVELS]);
+    const selectContainer = create('div', 'navigation__select-container', '', this.container);
+    create('label', 'navigation__select-label', GROUPS_OF_WORDS_TEXT, selectContainer);
+    this.select = create('select', 'navigation__group-select', '', selectContainer, ['title', DESCRIPTION_OF_LEVELS]);
     Array.from({ length: NUMBER_OF_GROUPS })
       .map((_, index) => index)
       .map((item) => create('option', 'navigation__option', String(item + 1), this.select, ['value', item]));
